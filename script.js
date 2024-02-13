05fdc62334310d31e953b5b37c11a430c249d5b9
AlexShtal <aleks2015.773@gmail.com>
2024-02-14 00:34:41 +0600

    add poshla nahui

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function to_fuck () {
     let kitty = document.getElementsByClassName("cat-gif")[0];
     kitty.src="https://c.tenor.com/qPurq2hheRoAAAAd/tenor.gif";
-    kitty.toggleAttribute("fuck-cat");
+    let text = document.querySelector("h1");
+    text.textContent = "Пошла нахуй";
 }
