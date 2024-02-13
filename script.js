8499b49776bbee356ecbaafb7fa1c4d0b2a4e135
AlexShtal <aleks2015.773@gmail.com>
2024-02-14 00:24:07 +0600

    init

---
+++ script.js
@@ -0,0 +1,5 @@
+function to_fuck () {
+    let kitty = document.getElementsByClassName("cat-gif")[0];
+    kitty.src="https://c.tenor.com/qPurq2hheRoAAAAd/tenor.gif";
+    kitty.toggleAttribute("fuck-cat");
+}
