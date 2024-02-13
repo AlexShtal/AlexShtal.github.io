43515c4e9a1e6b5faa9911667fb39954f690fa91
AlexShtal <aleks2015.773@gmail.com>
2024-02-14 00:36:24 +0600

    change encoding

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function to_fuck () {
     let kitty = document.getElementsByClassName("cat-gif")[0];
     kitty.src="https://c.tenor.com/qPurq2hheRoAAAAd/tenor.gif";
     let text = document.querySelector("h1");
-    text.textContent = "Пошла нахуй";
+    text.textContent = "РџРѕС€Р»Р° РЅР°С…СѓР№";
 }
